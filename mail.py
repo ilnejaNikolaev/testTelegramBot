399e7399a932c2c218c0338ab5ec7022da96cc79
Илья Николаев <ilneja046@gmail.com>
2026-04-11 12:47:51 +0300

    1.0

--- mail.py
+++ mail.py
@@ -6,5 +6,22 @@ api ='8632306485:AAEXOBNNKBYa-8wIWZ8kZpE9yQ3pfFjvSCA'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
+
+@dp.message_handler(text= ['Urban','ff'])
+async def urban_message(message):
+    print("Urban message")
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print("Start message")
+
+@dp.message_handler()
+async def all_message(message):
+    print("Мы получили сообщение!")
+
+
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
