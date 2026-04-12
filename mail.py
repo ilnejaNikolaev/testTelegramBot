83fbbd284e51972cc4d3499cc5a4705c145e125d
Илья Николаев <ilneja046@gmail.com>
2026-04-12 07:35:23 +0300

    1.1

--- mail.py
+++ mail.py
@@ -11,14 +11,19 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(text= ['Urban','ff'])
 async def urban_message(message):
     print("Urban message")
+    await message.answer('Urban message')
+
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
     print("Start message")
+    await message.answer('Рады видеть вас в нашем боте')
+
 
 @dp.message_handler()
 async def all_message(message):
     print("Мы получили сообщение!")
+    await message.answer(message.text)
 
 
 
