52d198982f985ae1bbd62b0cb214c9b23905931c
Илья Николаев <ilneja046@gmail.com>
2026-04-13 15:02:42 +0300

    1.3

--- mail.py
+++ mail.py
@@ -2,27 +2,27 @@ from aiogram import Dispatcher, executor, types, Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 import asyncio
 
 api ='8632306485:AAECgOimcdFFlVElc6wF5hdomxRu08BPnNI'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+kb = ReplyKeyboardMarkup()
+button = KeyboardButton(text='Информация')
+button2 = KeyboardButton(text='Начало')
+kb.add(button)
+kb.add(button2)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message):
+    await message.answer('Привет',reply_markup=kb)
 
-class UserState(StatesGroup):
-    adress = State()
+@dp.message_handler(text= 'Иформация')
+async def inform(message):
+    await message.answer('Инфомация о боте')
 
-@dp.message_handler(text= "заказать")
-async def buy(message):
-    await message.answer("Отправь нам свой адрес, пожалуйста")
-    await UserState.adress.set()
 
-@dp.message_handler(state= UserState.adress)
-async def fsm_handler(message,state):
-    await state.update_data(first = message.text)
-    data = await state.get_data()
-    await message.answer(f"Доставка будет отправленна на {data['first']}")
-    await state.finish()
 
 
 if __name__ == '__main__':
