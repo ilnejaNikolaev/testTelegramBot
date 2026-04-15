5a4bca3d76f24afa794e18f7e1954a93ad64bd67
Илья Николаев <ilneja046@gmail.com>
2026-04-15 13:39:09 +0300

    1.5

--- mail.py
+++ mail.py
@@ -3,26 +3,35 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+from aiogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 import asyncio
 
 api ='8632306485:AAECgOimcdFFlVElc6wF5hdomxRu08BPnNI'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True)
-button = KeyboardButton(text='Информация')
-button2 = KeyboardButton(text='Начало')
+kb = InlineKeyboardMarkup()
+button = InlineKeyboardButton(text='Информация', callback_data='info')
 kb.add(button)
-kb.add(button2)
+
+start_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='info')],
+        [
+            KeyboardButton(text='shop'),
+            KeyboardButton(text='donate'),
+        ]
+    ],resize_keyboard=True,
+)
+
 @dp.message_handler(commands=['start'])
-async def send_welcome(message):
-    print(f"Получена команда старт от {message.from_user.username}")
-    await message.answer('Привет',reply_markup=kb)
+async def send_welcome(message:types.Message):
+    await message.answer("Рады вас видеть",reply_markup=start_menu)
 
-@dp.message_handler(text= 'Иформация')
-async def inform(message):
-    await message.answer('Инфомация о боте')
-
+@dp.callback_query_handler(text='info')
+async def send_info(call):
+    await call.message.answer("Информация о боте")
+    await call.answer()
 
 
 
