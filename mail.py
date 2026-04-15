6a5f7fd99696e9bc9bf7b6acc550a6ee979a01c3
Илья Николаев <ilneja046@gmail.com>
2026-04-15 13:04:39 +0300

    1.4

--- mail.py
+++ mail.py
@@ -9,13 +9,14 @@ api ='8632306485:AAECgOimcdFFlVElc6wF5hdomxRu08BPnNI'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-kb = ReplyKeyboardMarkup()
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
 button = KeyboardButton(text='Информация')
 button2 = KeyboardButton(text='Начало')
 kb.add(button)
 kb.add(button2)
 @dp.message_handler(commands=['start'])
 async def send_welcome(message):
+    print(f"Получена команда старт от {message.from_user.username}")
     await message.answer('Привет',reply_markup=kb)
 
 @dp.message_handler(text= 'Иформация')
